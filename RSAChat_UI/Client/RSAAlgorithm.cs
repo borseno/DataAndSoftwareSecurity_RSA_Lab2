fe5ac0b94811656a4c513bb9a947b30d4dfb0006
Mark V <33373431+markvrc@users.noreply.github.com>
2020-04-09 13:45:01 +0300

    Add files via upload

---
+++ RSAChat_UI/Client/RSAAlgorithm.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace RSAChat_UI.Client
+{
+    public static class RSAAlgorithm
+    {
+        public static byte[] RSAEncrypt(byte[] plaintext, string destKey)
+        {
+            byte[] encryptedData;
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(destKey);
+            encryptedData = rsa.Encrypt(plaintext, true);
+            rsa.Dispose();
+            return encryptedData;
+        }
+
+        public static byte[] RSADecrypt(byte[] ciphertext, string srcKey)
+        {
+            byte[] decryptedData;
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            rsa.FromXmlString(srcKey);
+            decryptedData = rsa.Decrypt(ciphertext, true);
+            rsa.Dispose();  
+            return decryptedData;
+        }
+    }
+}
---
+++ RSAChat_UI/Server/ChatHub.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RSAChat_UI.Server
+{
+    public class ChatHub : Hub
+    {
+        private static readonly Dictionary<string, string> connectionPublicKey = new Dictionary<string, string>();
+        private static readonly Dictionary<string, string> connectionUsername = new Dictionary<string, string>();
+
+        public async Task SendMessage(string recipient, byte[] message)
+        {
+            var username = connectionUsername[Context.ConnectionId];
+
+            var isPM = recipient != null;
+
+            if (isPM == false)
+            {
+                await Clients.All.SendAsync("ReceiveMessage", username, message, isPM);
+            }
+            else
+            {
+                var recipientId = connectionUsername.First(i => i.Value == recipient).Key;
+                await Clients.Clients(recipientId, Context.ConnectionId).SendAsync("ReceiveMessage", username, message, true);
+            }
+        }
+
+        public async Task RegisterUser(string username, string publicKey)
+        {
+            connectionUsername.Add(Context.ConnectionId, username);
+            connectionPublicKey.Add(Context.ConnectionId, publicKey);
+            await Clients.All.SendAsync("NewConnection", connectionUsername[Context.ConnectionId], publicKey);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var usernamesKeys = connectionPublicKey.ToDictionary(i => connectionUsername[i.Key], i => i.Value);
+
+            await Clients.Client(Context.ConnectionId).SendAsync("GetParticipants", usernamesKeys);
+            await base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
